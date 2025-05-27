23e902141abc6c7f6f2a6e20fbfa838f0826ae00
UberSurgeon <yepplaz@gmail.com>
2025-05-27 15:01:40 +0300

    vite config commit

--- part2-notes-frontend/vite.config.js
+++ part2-notes-frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 5173,  // Use the PORT from Render or fallback to 5173 for local development
+    host: '0.0.0.0',  // Make the server accessible on any network interface
+  },
 })
+
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
